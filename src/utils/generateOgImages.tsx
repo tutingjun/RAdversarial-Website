61bffaf280ce58e135f93ab2c02f2a05710eacb8
tutingjun <50512161+tutingjun@users.noreply.github.com>
2024-02-14 15:00:59 -0600

    Reformat for our project use

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -1,7 +1,6 @@
 import satori, { type SatoriOptions } from "satori";
 import { Resvg } from "@resvg/resvg-js";
 import { type CollectionEntry } from "astro:content";
-import postOgImage from "./og-templates/post";
 import siteOgImage from "./og-templates/site";
 
 const fetchFonts = async () => {
@@ -46,11 +45,6 @@ function svgBufferToPngBuffer(svg: string) {
   const resvg = new Resvg(svg);
   const pngData = resvg.render();
   return pngData.asPng();
-}
-
-export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
-  const svg = await satori(postOgImage(post), options);
-  return svgBufferToPngBuffer(svg);
 }
 
 export async function generateOgImageForSite() {
